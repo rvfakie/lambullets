4f9082b5207dd03e4c540e210b8415ef17000fa1
Alexander Khmara <khmaraalex@gmail.com>
2018-07-20 17:38:22 +0300

    create header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    Hello world
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+}
+
+</script>
+
+<style lang="scss" scoped>
+ .header{
+   height: 100px;
+ }
+</style>
