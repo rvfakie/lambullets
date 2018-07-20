8814fa39b4d64901a51b77caffb562eeef650ee8
Alexander Khmara <khmaraalex@gmail.com>
2018-07-20 17:46:34 +0300

    add scss

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
+    <Header />
     <Index />
   </div>
 </template>
 
 <script>
 import Index from './components/Content/Index'
+import Header from './components/Header/Header'
 
 export default {
   name: 'App',
-  components: { Index }
+  components: { Index, Header }
 }
 </script>
 
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    Hello world
+    Hello header
   </div>
 </template>
 
